cd1740a365e13d04206b4cb20cd615171d948ce3
Dash <dasha.semivolos@gmail.com>
2022-12-23 19:46:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//ПР10_2ИСП2_Сейдалиев_Семиволос_Вариант-2
+using System;
+
+namespace ПР10_2ИСП2_Сейдалиев_Семиволос
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/*Console.WriteLine("Введите номер  метода: ");
+			int  n  =  Convert.ToInt32(Console.ReadLine()); 
+			
+			int  a = Convert.ToInt32(Console.ReadLine());
+			int  b = Convert.ToInt32(Console.ReadLine());
+			double dx, y = Convert.ToDouble(Console.ReadLine());
+			
+			switch (n)
+			{
+				case 1:
+					method1();
+					break;
+				case 2:
+					method2();
+					break;
+				case 3:
+					method3();
+					break;
+			}*/
+		}
+		static void method1()
+		{	
+			int a = x;
+			while (x<= b)
+			{
+				y = x * x - 3;
+				Console.WriteLine(y);
+			}		
+		}
+		static void method2()
+		{
+			do
+			while ()
+		}
+		static void method3()
+		{
+			int  a = Convert.ToInt32(Console.ReadLine());
+			int  b = Convert.ToInt32(Console.ReadLine());
+			double dx, y = Convert.ToDouble(Console.ReadLine());
+			for (x = a; x<= b; x+=dx)
+			{
+				y = x * x - 3;
+				Console.WriteLine(y);
+			}
+				
+		}
+	}
+}
