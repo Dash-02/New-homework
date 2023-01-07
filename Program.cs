9fddfa68e8a2e5c7a67c63c606cc3bcdf05c618a
Dash-02 <102436712+Dash-02@users.noreply.github.com>
2023-01-07 22:34:50 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,53 +5,117 @@ namespace ПР10_2ИСП2_Сейдалиев_Семиволос
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		static void Method1()
+        	{
+			Console.Write("Укажите интервал [a, b] \nВведите a: ");
+           		double a = Convert.ToDouble(Console.ReadLine());
+
+           		Console.Write("Введите b: ");
+           		double b = Convert.ToDouble(Console.ReadLine());
+
+		   	Console.Write("Введите шаг: ");
+		   	double dx = Convert.ToDouble(Console.ReadLine());
+
+		   	double y, x = a;
+
+		  	 for (int i = 0; i < 35; i++) Console.Write("-");
+		   	Console.WriteLine();
+		   	Console.Write("\tx\t|\ty");
+		   	Console.WriteLine();
+		   	for (int i = 0; i < 35; i++) Console.Write("-");
+		   	Console.WriteLine();
+
+		    	while (x <= b)
+		    	{
+		        	y = x * x - 3;
+		        	Console.WriteLine($"\t{x:f1}\t|\t{y:f1}");
+				x += dx;
+		    	}
+        	}
+
+		static void Method2()
 		{
-			/*Console.WriteLine("Введите номер  метода: ");
-			int  n  =  Convert.ToInt32(Console.ReadLine()); 
-			
-			int  a = Convert.ToInt32(Console.ReadLine());
-			int  b = Convert.ToInt32(Console.ReadLine());
-			double dx, y = Convert.ToDouble(Console.ReadLine());
-			
-			switch (n)
-			{
-				case 1:
-					method1();
-					break;
-				case 2:
-					method2();
-					break;
-				case 3:
-					method3();
-					break;
-			}*/
+		    Console.Write("Укажите интервал [a, b] \nВведите a: ");
+		    double a = Convert.ToDouble(Console.ReadLine());
+
+		    Console.Write("Введите b: ");
+		    double b = Convert.ToDouble(Console.ReadLine());
+
+		    Console.Write("Введите шаг: ");
+		    double dx = Convert.ToDouble(Console.ReadLine());
+
+		    double y, x = a;
+
+		    for (int i = 0; i < 35; i++) Console.Write("-");
+		    Console.WriteLine();
+		    Console.Write("\tx\t|\t y");
+		    Console.WriteLine();
+		    for (int i = 0; i < 35; i++) Console.Write("-");
+		    Console.WriteLine();
+
+		    do
+		    {
+			y = x * x - 3;
+			Console.WriteLine($"\t{x:f1}\t|\t{y:f1}");
+			x += dx;
+		    } 
+		    while (x <= b);
 		}
-		static void method1()
-		{	
-			int a = x;
-			while (x<= b)
-			{
-				y = x * x - 3;
-				Console.WriteLine(y);
-			}		
-		}
-		static void method2()
+		
+		static void Method3()
 		{
-			do
-			while ()
+		    Console.Write("Укажите интервал [a, b] \nВведите a: ");
+		    double a = Convert.ToDouble(Console.ReadLine());
+
+		    Console.Write("Введите b: ");
+		    double b = Convert.ToDouble(Console.ReadLine());
+
+		    Console.Write("Введите шаг: ");
+		    double dx = Convert.ToDouble(Console.ReadLine());
+
+		    double x, y;
+
+		    for (int i = 0; i < 35; i++) Console.Write("-");
+		    Console.WriteLine();
+		    Console.Write("\tx\t|\ty");
+		    Console.WriteLine();
+		    for (int i = 0; i < 35; i++) Console.Write("-");
+		    Console.WriteLine();
+
+		    for (x = a; x <= b; x += dx)
+		    {
+			y = x * x - 3;
+			Console.WriteLine($"\t{x:f1}\t|\t{y:f1}");
+		    }
 		}
-		static void method3()
+
+		public static void Main(string[] args)
 		{
-			int  a = Convert.ToInt32(Console.ReadLine());
-			int  b = Convert.ToInt32(Console.ReadLine());
-			double dx, y = Convert.ToDouble(Console.ReadLine());
-			for (x = a; x<= b; x+=dx)
-			{
-				y = x * x - 3;
-				Console.WriteLine(y);
-			}
-				
+		    Console.Write("Введите номер метода решения: ");
+		    int n = Convert.ToInt32(Console.ReadLine());
+
+		    switch (n)
+		    {
+			case 1:
+			    Method1();
+			    break;
+
+			case 2:
+			    Method2();
+			    break;
+
+			case 3:
+			    Method3();
+			    break;
+
+			default:
+			    Console.WriteLine("Введен неверный номер метода");
+			    break;
+		    }
+		    for (int i = 0; i < 35; i++) Console.Write("-");
+
+		    Console.Write("\nНажмите Enter для завершения программы.");
+		    Console.ReadKey();
 		}
 	}
 }
